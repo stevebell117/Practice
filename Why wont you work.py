897d258f1a70adf9c18025ff2edd0bb8dc27d663
CelestialNebula <41875671+CelestialNebula@users.noreply.github.com>
2018-09-13 15:18:23 +0000

    Add files via upload

---
+++ Why wont you work.py
@@ -0,0 +1,8 @@
+# Work plz.txt = Random letters in a txt file
+f = open("C:\\Users\\Snows\\Desktop\\Work plz.txt")
+f.readlines()
+f.close()
+# f = open("C:\\Users\\Snows\\Desktop\\Work plz.txt")
+# for line in f:
+#    print(line)
+# f.close()
