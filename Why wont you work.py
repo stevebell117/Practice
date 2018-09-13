c552e3463703575ca78d2abc3cdea21ce3b54e8a
Steven Bell <steven11@mail.usf.edu>
2018-09-13 10:48:34 -0500

    Update Why wont you work.py
    Run this and send me what it throws

--- Why wont you work.py
+++ Why wont you work.py
@@ -1,8 +1,15 @@
+from pprint import pprint
+import traceback
 # Work plz.txt = Random letters in a txt file
 # First one is not printing but the second is
-f = open("C:\\Users\\Snows\\Desktop\\Work plz.txt")
-f.readlines()
-f.close()
+# First one
+try:
+    f = open("C:\\Users\\Snows\\Desktop\\Work plz.txt")
+    f.readlines()
+    f.close()
+except:
+    pprint(traceback.print_exc())  
+# Second one
 # f = open("C:\\Users\\Snows\\Desktop\\Work plz.txt")
 # for line in f:
 #    print(line)
